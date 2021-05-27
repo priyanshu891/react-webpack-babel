c1e1e202c2bab10d1a3e2fa574986405ce724f6c
priyanshu rai <priyanshu.rai891@gmail.com>
2021-05-28 00:48:16 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h1>Hello World from webpack</h1>;
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.bundle.js",
+  },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "src"),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 9876,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
