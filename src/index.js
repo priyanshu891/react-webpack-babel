c615df22fb0b895a150592e8f5d57f665f1433df
priyanshu rai <priyanshu.rai891@gmail.com>
2021-05-28 00:54:04 +0530

    Checked

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const HelloWorld = () => {
-  return <h1>Hello World from webpack</h1>;
+  return <h1>Hello World from Webpack and Babel</h1>;
 };
 
 ReactDOM.render(<HelloWorld />, document.getElementById("root"));
