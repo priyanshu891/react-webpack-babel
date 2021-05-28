2360f8a966f88494e0f1ea4424b73f447660a926
priyanshu rai <priyanshu.rai891@gmail.com>
2021-05-28 15:36:26 +0530

    Change in configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-const HelloWorld = () => {
-  return <h1>Hello World from Webpack and Babel</h1>;
-};
-
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<App name="Priyanshu" />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   mode: process.env.NODE_ENV || "development",
   resolve: {
     modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".js", ".jsx"],
   },
   devServer: {
     contentBase: path.join(__dirname, "src"),
